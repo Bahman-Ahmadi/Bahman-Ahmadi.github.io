961c845a732337d365e52d32f5de8911ede4c643
Bahman <bahmanahmadi.mail@gmail.com>
2023-02-23 21:10:46 +0330

    soundload CLIENT folder increased

---
+++ soundload/CLIENT/app/src/main/java/ir/bpr/soundload/ui/activity/Prefs.java
@@ -0,0 +1,15 @@
+package ir.bpr.soundload.ui.activity;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
+
+public class Prefs extends PreferenceActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+    }
+}
