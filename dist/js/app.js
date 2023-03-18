96f0330d3bee4a0a3497184ea28b098167ea233a
Bahman <bahmanahmadi.mail@gmail.com>
2023-03-18 13:39:49 +0330

    Files have been added

---
+++ dist/js/app.js
@@ -0,0 +1,13 @@
+new fullpage('#fullpage', {
+    //options here
+    autoScrolling:true,
+    scrollBar: true
+});
+document.querySelectorAll("a").forEach((el) => {
+    if (el.href == "https://alvarotrigo.com/fullPage/") {
+        el.remove();
+    }
+});
+particlesJS.load('c1', 'dist/assets/particles.json', function() {
+    console.log('callback - particles.js config loaded');
+});
