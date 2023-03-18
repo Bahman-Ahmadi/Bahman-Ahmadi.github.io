2fccbb0f00fd3d276a96fff7d9caddd2344bcbb4
Bahman <bahmanahmadi.mail@gmail.com>
2023-03-18 20:42:13 +0330

    Files have been updated

--- dist/js/app.js
+++ dist/js/app.js
@@ -11,3 +11,41 @@ document.querySelectorAll("a").forEach((el) => {
 particlesJS.load('c1', 'dist/assets/particles.json', function() {
     console.log('callback - particles.js config loaded');
 });
+
+var db = {}
+$.ajax({
+	type: "GET",
+	url: "dist/db.json",
+	datatype: "json",
+	async: false,
+	success: function(data){
+		db = data;
+	}
+});
+
+document.querySelectorAll(".bio").forEach((el) => {
+    el.innerHTML = db.bio;
+});
+
+document.querySelectorAll(".about-me").forEach((el) => {
+    el.innerHTML = db.about;
+});
+
+db.projects.forEach((project) => {
+    $(".projects-list")[0].innerHTML = `
+<div class="project col-xs-12 col-sm-12 col-md-5 col-md-5">
+    <div class="project-header d-flex">
+        <img src="${project.logo}" alt="rubika" class="project-logo">
+        <div class="project-header-texts">
+            <p class="project-name">${project.name}</p>
+            <p class="project-lang">${project.lang}</p>
+        </div>
+    </div>
+    <div class="project-content">
+        <p class="project-date">ساخته شده در ${project.date}</p>
+    </div>
+    <div class="project-line"></div>
+    <a href="${project.link}"><button class="project-view">اطلاعات بیشتر</button></a>
+</div>
+    `;
+});
