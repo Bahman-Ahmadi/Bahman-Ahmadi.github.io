5143577ed27501057b6addffb3e18f42a4c0f5d7
Bahman <bahmanahmadi.mail@gmail.com>
2023-03-18 16:26:11 +0330

    Files have been updated

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,7 +1,7 @@
 new fullpage('#fullpage', {
     //options here
-    autoScrolling:true,
-    scrollBar: true
+    autoScrolling: true,
+    scrollBar: false
 });
 document.querySelectorAll("a").forEach((el) => {
     if (el.href == "https://alvarotrigo.com/fullPage/") {
