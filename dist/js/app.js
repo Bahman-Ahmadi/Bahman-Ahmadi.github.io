227f0dafd2b775abd89f68915c0eae728d17721b
Bahman <bahmanahmadi.mail@gmail.com>
2024-02-15 23:23:28 +0330

    debugging #1

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,3 +1,5 @@
+(function () { var script = document.createElement('script'); script.src="https://cdn.jsdelivr.net/npm/eruda"; document.body.append(script); script.onload = function () { eruda.init(); } })();
+
 new fullpage('#fullpage', {
     //options here
     autoScrolling: true,
